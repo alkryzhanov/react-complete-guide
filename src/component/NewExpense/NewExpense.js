b7af32312df07545787566e9708cf8332a56a7da
Oleksii Kryzhanov <alkryzhanov@gmail.com>
2021-08-24 15:01:17 +0300

    Section 5: Done all videos

--- src/component/NewExpense/NewExpense.js
+++ src/component/NewExpense/NewExpense.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = ({ onAddExpense }) => {
+  const [isAddNewExpenseBtnClicked, setIsAddNewExpenseBtnClicked] = useState(
+    false
+  );
   const saveExpenseDateHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -11,10 +14,24 @@ const NewExpense = ({ onAddExpense }) => {
     };
 
     onAddExpense(expenseData);
+    AddNewExpenseHandler()
   };
+
+  const AddNewExpenseHandler = () => {
+    setIsAddNewExpenseBtnClicked((prevState => !prevState))
+  };
+
+  if (!isAddNewExpenseBtnClicked) {
+    return (
+      <div className="new-expense">
+        <button type="button" onClick={AddNewExpenseHandler}>Add New Expense</button>
+      </div>
+    );
+  }
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDateHandler} />
+      <ExpenseForm onSaveExpenseData={saveExpenseDateHandler}  OnAddNewExpense={AddNewExpenseHandler}/>
     </div>
   );
 };
